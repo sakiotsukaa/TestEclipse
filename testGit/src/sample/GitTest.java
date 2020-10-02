15342f206cac8adbc442aacba791cdd23a90232d
sakiotsukaa <saki.ootsuka.555@gmail.com>
2020-10-02 17:47:43 +0900

    changed message

--- testGit/src/sample/GitTest.java
+++ testGit/src/sample/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
+		for(int i = 0; i < 10; i++) {
+			System.out.println("yay!");
+		}
 	}
 
 }
